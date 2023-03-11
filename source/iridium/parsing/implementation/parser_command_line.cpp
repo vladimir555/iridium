bc086be29481ad6e2211fd82a2ebd9833dde62cf
volodja <bulaev_vladimir@mail.ru>
2023-03-12 05:54:56 +0800

    - refactoring

--- source/iridium/parsing/implementation/parser_command_line.cpp
+++ source/iridium/parsing/implementation/parser_command_line.cpp
@@ -1,47 +1,47 @@
-#include "parser_command_line.h"
+//#include "parser_command_line.h"
 
 
-#include "iridium/strings.h"
-#include "node.h"
+//#include "iridium/strings.h"
+//#include "node.h"
 
-#include <string_view>
-
-
-namespace iridium {
-namespace parsing {
-namespace implementation {
+//#include <string_view>
 
 
-INode::TSharedPtr CCommandLineParser::parse(std::string const &source) const {
-    INode::TSharedPtr node = CNode::create("root");
-
-    for (std::string_view item: split(source, " ")) {
-        if (item.substr(0, 2) == "--") {
-            item.remove_prefix(2);
-            auto i = item.find('=');
-            if (i == std::string::npos)
-                throw std::runtime_error("command line parsing error: argument '" + std::string(item) + "', source '" + source + "'");
-            node->addChild(std::string(item.substr(0, i)), std::string(item.substr(i + 1)));
-            continue; // <---
-        }
-        if (item.substr(0, 1) == "-") {
-            item.remove_prefix(1);
-            if (item.size() == 1 && std::isalpha(item.back()))
-                node->addChild(std::string(item), convertion::convert<std::string>(true));
-            else
-                throw std::runtime_error("command line parsing error: argument '" + std::string(item) + "', source '" + source + "'");
-        }
-    }
-
-    return node; // ----->
-}
+//namespace iridium {
+//namespace parsing {
+//namespace implementation {
 
 
-std::string CCommandLineParser::compose(INode::TConstSharedPtr const &root_node) const {
-    return "";
-}
+//INode::TSharedPtr CCommandLineParser::parse(std::string const &source) const {
+//    INode::TSharedPtr node = CNode::create("root");
+
+//    for (std::string_view item: split(source, " ")) {
+//        if (item.substr(0, 2) == "--") {
+//            item.remove_prefix(2);
+//            auto i = item.find('=');
+//            if (i == std::string::npos)
+//                throw std::runtime_error("command line parsing error: argument '" + std::string(item) + "', source '" + source + "'");
+//            node->addChild(std::string(item.substr(0, i)), std::string(item.substr(i + 1)));
+//            continue; // <---
+//        }
+//        if (item.substr(0, 1) == "-") {
+//            item.remove_prefix(1);
+//            if (item.size() == 1 && std::isalpha(item.back()))
+//                node->addChild(std::string(item), convertion::convert<std::string>(true));
+//            else
+//                throw std::runtime_error("command line parsing error: argument '" + std::string(item) + "', source '" + source + "'");
+//        }
+//    }
+
+//    return node; // ----->
+//}
 
 
-} // implementation
-} // parsing
-} // iridium
+//std::string CCommandLineParser::compose(INode::TConstSharedPtr const &root_node) const {
+//    return "";
+//}
+
+
+//} // implementation
+//} // parsing
+//} // iridium
--- source/iridium/parsing/implementation/parser_command_line.h
+++ source/iridium/parsing/implementation/parser_command_line.h
@@ -1,33 +1,33 @@
-// Copyright © 2019 Bulaev Vladimir.
-// Contacts: <bulaev_vladimir@mail.ru>
-// License: https://www.gnu.org/licenses/lgpl-3.0
+//// Copyright © 2019 Bulaev Vladimir.
+//// Contacts: <bulaev_vladimir@mail.ru>
+//// License: https://www.gnu.org/licenses/lgpl-3.0
 
-#ifndef HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
-#define HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
-
-
-#include "iridium/smart_ptr.h"
-#include "iridium/parsing/node.h"
-#include "iridium/parsing/parser.h"
+//#ifndef HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
+//#define HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
 
 
-namespace iridium {
-namespace parsing {
-namespace implementation {
+//#include "iridium/smart_ptr.h"
+//#include "iridium/parsing/node.h"
+//#include "iridium/parsing/parser.h"
 
 
-class CCommandLineParser: public IParser {
-public:
-    DEFINE_IMPLEMENTATION(CCommandLineParser)
-    CCommandLineParser() = default;
-    INode::TSharedPtr parse(std::string const &source) const override;
-    std::string compose(INode::TConstSharedPtr const &root_node) const override;
-};
+//namespace iridium {
+//namespace parsing {
+//namespace implementation {
 
 
-} // implementation
-} // parsing
-} // iridium
+//class CCommandLineParser: public IParser {
+//public:
+//    DEFINE_IMPLEMENTATION(CCommandLineParser)
+//    CCommandLineParser() = default;
+//    INode::TSharedPtr parse(std::string const &source) const override;
+//    std::string compose(INode::TConstSharedPtr const &root_node) const override;
+//};
 
 
-#endif // HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
+//} // implementation
+//} // parsing
+//} // iridium
+
+
+//#endif // HEADER_PARSER_COMMAND_LINE_BD6035D1_9318_4137_8F01_90F9EA796AF2
