45865318f03604d86a2b0625fff7611d275c34fc
volodja <bulaev_vladimir@mail.ru>
2023-03-20 11:58:05 +0800

    - refactoring

--- source/iridium/testing/implementation/unit_test_session_factory.cpp
+++
@@ -1,9 +0,0 @@
-#include "unit_test_session_factory.h"
-
-
-namespace iridium {
-namespace testing {
-namespace implementation {
-} // implementation
-} // testing
-} // iridium
--- source/iridium/testing/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-
-
-namespace iridium {
-namespace testing {
-} // testing
-} // iridium
