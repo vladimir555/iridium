39e14ee297b9356da820c34cd5c62744f8da9d3e
volodja <bulaev_vladimir@mail.ru>
2023-08-10 20:49:04 +0800

    - logging: file sink rotation and init error output

--- source/iridium/logging/implementation/sink_file.cpp
+++ source/iridium/logging/implementation/sink_file.cpp
@@ -3,6 +3,9 @@
 #include "iridium/convertion/convert.h"
 #include "iridium/io/fs/implementation/file_stream_writer.h"
 #include "iridium/threading/implementation/worker.h"
+#include "iridium/items.h"
+
+#include <iostream>
 
 
 using iridium::convertion::convert;
@@ -14,17 +17,20 @@ using std::chrono::system_clock;
 using std::string;
 
 
+IMPLEMENT_ENUM(iridium::logging::implementation::CSinkFile::TRotation)
+
+
 namespace iridium {
 namespace logging {
 namespace implementation {
 
 
-CSinkFile::CSinkFile(TEvent::TLevel const &level, string const &file_name)
+CSinkFile::CSinkFile(TEvent::TLevel const &level, string const &path, TRotation const &rotation)
 :
     m_level                     (level),
-    m_file_name                 (file_name),
-    m_file_name_original        (file_name),
-    m_is_rotation_by_day        (true), // todo: rotation time 1h, 1d, 1w ...
+    m_path                      (path),
+    m_path_original             (path),
+    m_rotation                  (rotation),
     m_last_initialization_time  (),
     m_file_writer               (nullptr)
 {}
@@ -34,28 +40,62 @@ void CSinkFile::initialize() {
     if (m_file_writer)
         return; // ----->
     
-    if (m_is_rotation_by_day) {
-        auto date                   = convert<string>(system_clock::now()).substr(0, 10);
-        m_last_initialization_time  = convert<system_clock::time_point>(date + " 00:00:00.000");
+    if (checkOneOf(m_rotation, TRotation::DAILY, TRotation::TIMELY)) {
+        string date;
 
-        // todo: bugfix for path "./"
-        m_file_name = m_file_name_original;
-        auto file_ext_position = m_file_name.find_last_of('.');
-        if  (file_ext_position > 0)
-            m_file_name.replace (m_file_name.find_last_of('.'), 1, "-" + date + ".");
+        if (m_rotation == TRotation::DAILY) {
+            date = convert<string>(system_clock::now()).substr(0, 10);
+            m_last_initialization_time = convert<system_clock::time_point>(date + " 00:00:00.000");
+        }
+
+        if (m_rotation == TRotation::TIMELY) {
+            auto now    = system_clock::now();
+            date        = convert<string>(now);
+            date[10]    = '_';
+            date[13]    = '-';
+            date[16]    = '-';
+            m_last_initialization_time = now;
+        }
+
+        m_path = m_path_original;
+
+        m_path.erase(std::remove(m_path.begin(), m_path.end(), '\\'), m_path.end());
+        std::replace(m_path.begin(), m_path.end(), '\\', '/');
+
+        auto file_ext_position = m_path.find_last_of('/');
+        if  (file_ext_position == string::npos)
+             file_ext_position = 0;
+
+        file_ext_position += m_path.substr(file_ext_position).find_last_of('.');
+
+        if  (file_ext_position == string::npos)
+            m_path += "-" + date;
+
         else
-            m_file_name.replace (m_file_name.find_last_of('.'), 1, date + ".");
+
+        if  (file_ext_position > 0)
+            m_path.replace(file_ext_position, 1, "-" + date + ".");
+        else
+            m_path.replace(file_ext_position, 1, date + ".");
     }
 
-    m_file_writer = CFileStreamWriter::create(m_file_name);
-    m_file_writer->initialize();
+    try {
+        m_file_writer = CFileStreamWriter::create(m_path);
+        m_file_writer->initialize();
+    } catch (std::exception const &e) {
+        std::cerr << "initializing logger file sink '" + m_path + "' error: " << e.what() << std::endl;
+    }
 }
 
 
 void CSinkFile::finalize() {
     if (m_file_writer) {
-        m_file_writer->finalize();
-        m_file_writer.reset();
+        try {
+            m_file_writer->finalize();
+            m_file_writer.reset();
+        } catch (std::exception const &e) {
+            std::cerr << "finalizing logger file sink '" + m_path + "' error: " << e.what() << std::endl;
+        }
     }
 }
 
@@ -64,7 +104,7 @@ void CSinkFile::log(TEvent::TConstSharedPtr const &event) {
     if (!m_file_writer || event->level < m_level)
         return; // ----->
 
-    if (m_is_rotation_by_day && system_clock::now() > m_last_initialization_time) {
+    if (m_rotation == TRotation::DAILY && system_clock::now() > m_last_initialization_time) {
         finalize();
         initialize();
     }
