a2dda51c765409b5a848285cb3187fb62ccff72b
volodja <bulaev_vladimir@mail.ru>
2024-03-15 13:02:02 +0800

    - threading: mutexes tracing feature

--- source/iridium/threading/implementation/async_queue.h
+++ source/iridium/threading/implementation/async_queue.h
@@ -19,13 +19,13 @@ namespace threading {
 namespace implementation {
 
 
-// ----- interface
+// ----- class definidion
 
 
 template<typename TItem>
 class CAsyncQueue:
     public  IAsyncQueue<TItem>,
-    private Synchronized<std::mutex, true>,
+    private Synchronized<std::mutex>,
     private pattern::NonCopyable
 {
 public:
@@ -47,144 +47,133 @@ public:
     
 private:
     std::list<TItem>    m_items;
-    std::atomic<bool>   m_is_waiting;
+//    std::atomic<bool>   m_is_waiting;
     std::atomic<bool>   m_is_empty;
 };
 
 
-// ----- implementation
+// ----- class implementation
 
 
 template<typename TItem>
 CAsyncQueue<TItem>::CAsyncQueue()
 :
-    m_is_waiting(true),
+//    m_is_waiting(true),
     m_is_empty  (true)
 {}
 
 
 template<typename TItem>
 size_t CAsyncQueue<TItem>::push(TItem const &item) {
-//    LOCK_SCOPE();
-//
-//    m_items.push_back(item);
-//    m_is_empty = m_items.empty();
-//
-//    LOGT << "M: " << m_mutex_name << " push items, size: " << m_items.size() << ", m_is_empty: " << m_is_empty;
-//
-//    return m_items.size();
-
     {
-        std::unique_lock<std::mutex> l(m_mutex);
-
+        LOCK_SCOPE();
         m_items.push_back(item);
         m_is_empty = m_items.empty();
         auto size  = m_items.size();
 
-        l.unlock();
-        m_cv.notify_one();
-
         return size; // ----->
     }
+
+
+//    {
+//        std::unique_lock<std::mutex> l(m_mutex);
+//
+//        m_items.push_back(item);
+//        m_is_empty = m_items.empty();
+//        auto size  = m_items.size();
+//
+//        l.unlock();
+//        m_cv.notify_one();
+//
+//        return size; // ----->
+//    }
 }
 
 
 template<typename TItem>
 size_t CAsyncQueue<TItem>::push(std::list<TItem> const &items) {
-//    LOCK_SCOPE();
-//
-//    m_items.insert(m_items.end(), items.begin(), items.end());
-//    m_is_empty = m_items.empty();
-//
-//    LOGT << "M: " << m_mutex_name << " push item, size: " << m_items.size() << ", m_is_empty: " << m_is_empty;
-//
-//    return m_items.size();
-    
-    
     {
-        std::unique_lock<std::mutex> l(m_mutex);
+        LOCK_SCOPE();
 
         m_items.insert(m_items.end(), items.begin(), items.end());
-        m_is_empty  = m_items.empty();
-        auto size   = m_items.size();
+        m_is_empty = m_items.empty();
 
-        l.unlock();
-        m_cv.notify_one();
-
-        return size; // ----->
+        return m_items.size();
     }
+
+    
+//    {
+//        std::unique_lock<std::mutex> l(m_mutex);
+//
+//        m_items.insert(m_items.end(), items.begin(), items.end());
+//        m_is_empty  = m_items.empty();
+//        auto size   = m_items.size();
+//
+//        l.unlock();
+//        m_cv.notify_one();
+//
+//        return size; // ----->
+//    }
 }
 
 
 template<typename TItem>
 std::list<TItem> CAsyncQueue<TItem>::pop(bool const &is_waiting) {
-//    if (m_is_waiting && m_is_empty && is_waiting) {
-//        LOGT << "M: " << m_mutex_name << " pop waiting, m_is_waiting: " << m_is_waiting
-//             << ", m_is_empty: " << m_is_empty
-//             << ", is_waiting: " << is_waiting;
-//
-//        LOCK_SCOPE_WAIT() {
-//            m_is_empty = true;
-//            LOGT << "pop waiting, size: " << m_items.size();
-//            return std::move(m_items); // ----->
-//        }
-//    }
-//
-//    LOCK_SCOPE();
-//    m_is_empty = true;
-//    return std::move(m_items); // ----->
-    
     {
-        std::unique_lock<std::mutex> l(m_mutex);
-        while (m_is_waiting && m_is_empty && is_waiting)
-            m_cv.wait(l);
+        LOCK_SCOPE();
+        if (m_is_empty && is_waiting)
+            LOCK_SCOPE_TRY_WAIT();
 
         m_is_empty = true;
         return std::move(m_items); // ----->
     }
+
+//    {
+//        std::unique_lock<std::mutex> l(m_mutex);
+//        while (m_is_waitable && m_is_empty && is_waiting)
+//            m_cv.wait(l);
+//
+//        m_is_empty = true;
+//        return std::move(m_items); // ----->
+//    }
 }
 
 
 template<typename TItem>
 std::list<TItem> CAsyncQueue<TItem>::pop(std::chrono::nanoseconds const &timeout) {
-//    if (m_is_waiting && m_is_empty) {
-//        LOGT << "M: " << m_mutex_name << " pop timed, m_is_waiting: " << m_is_waiting
-//             << ", m_is_empty: " << m_is_empty;
-//
-//        LOCK_SCOPE_WAIT(timeout) {
-//            m_is_empty = true;
-//            LOGT << "pop timed, size: " << m_items.size();
-//            return std::move(m_items); // ----->
-//        }
-//    }
-//
-//    LOCK_SCOPE();
-//    m_is_empty = true;
-//    return std::move(m_items);
-    
-
     {
-        std::unique_lock<std::mutex> l(m_mutex);
+        LOCK_SCOPE();
 
-        if (m_is_waiting && m_is_empty)
-            m_cv.wait_for(l, timeout);
+        if (m_is_empty)
+            LOCK_SCOPE_TRY_WAIT(timeout);
 
         m_is_empty = true;
 
         return std::move(m_items); // ----->
     }
+
+
+//    {
+//        std::unique_lock<std::mutex> l(m_mutex);
+//
+//        if (m_is_waitable && m_is_empty)
+//            m_cv.wait_for(l, timeout);
+//
+//        m_is_empty = true;
+//
+//        return std::move(m_items); // ----->
+//    }
 }
 
 
 template<typename TItem>
 void CAsyncQueue<TItem>::interrupt() {
-//    m_is_waiting = false;
-//    Synchronized::interrupt();
+    Synchronized::interrupt();
 
-    {
-        m_is_waiting = false;
-        m_cv.notify_all();
-    }
+//    {
+//        m_is_waiting = false;
+//        m_cv.notify_all();
+//    }
 }
 
 
