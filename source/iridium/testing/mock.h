73592a7b6937f68623091476f9c95c068c9fb2e9
volodja <bulaev_vladimir@mail.ru>
2025-04-22 00:54:49 +0800

    - testing: mock example

---
+++ source/iridium/testing/mock.h
@@ -0,0 +1,29 @@
+//#ifndef HEADER_MOCK_67D176F4_9136_4225_974D_B12E2C3C7BC2
+//#define HEADER_MOCK_67D176F4_9136_4225_974D_B12E2C3C7BC2
+//
+//
+//#include "iridium/smart_ptr.h"
+//
+//#include <string>
+//#include <functional>
+//
+//
+//namespace iridium {
+//namespace testing {
+//
+//
+//class IMock {
+//public:
+//    DEFINE_INTERFACE(IMock);
+//
+//    virtual void setupBehavior  (std::string const &method_name, std::function<void()> const &behavior) = 0;
+//    virtual void callMethod     (std::string const &method_name) = 0;
+//    virtual void verifyCall     (std::string const &method_name, int const &expected_calls = 1) const = 0;
+//};
+//
+//
+//} // testing
+//} // iridium
+//
+//
+//#endif // HEADER_MOCK_67D176F4_9136_4225_974D_B12E2C3C7BC2
