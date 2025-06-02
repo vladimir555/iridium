533bd85b781e2d3efa3cdcc8a966b78b91453106
volodja <bulaev_vladimir@mail.ru>
2025-06-02 12:50:19 +0800

    - logging: release logs optimization

--- source/iridium/logging/logger.h
+++ source/iridium/logging/logger.h
@@ -59,10 +59,10 @@ private:
 };
 
 
-struct LogStreamDummy {
-    template<typename TValue>
-    LogStreamDummy const & operator << (TValue v) const;
-};
+//struct LogStreamDummy {
+//    template<typename TValue>
+//    LogStreamDummy const & operator << (TValue v) const;
+//};
 
 
 ///
@@ -135,12 +135,6 @@ private:
     ///
     mutable TEvent::TSharedPtr m_event;
 };
-
-
-template<typename TValue>
-LogStreamDummy const &LogStreamDummy::operator << (TValue) const {
-    return *this;
-}
 
 
 template<typename TValue>
@@ -283,16 +277,14 @@ std::string extractFileNameToLog(std::string const &path);
 iridium::logging::LogStream(iridium::logging::TEvent::TLevel::TRACE) << \
 iridium::logging::extractFileNameToLog(std::string(__FILE__) + ":" + std::to_string(__LINE__)) << " "
 #else
-#define LOGT \
-iridium::logging::LogStreamDummy()
+#define LOGT //
 #endif // BUILD_TYPE_DEBUG
 
 #if defined(BUILD_TYPE_DEBUG) || defined(BUILD_FLAG_FORCE_DEBUG_LOG)
 #define LOGD \
 iridium::logging::LogStream(iridium::logging::TEvent::TLevel::DEBUG)
 #else
-#define LOGD \
-iridium::logging::LogStreamDummy()
+#define LOGD //
 #endif // BUILD_TYPE_DEBUG
 
 #define LOGI \
