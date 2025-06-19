959bd78ae93cb952794ffd2f268397e49e7aa7c7
volodja <bulaev_vladimir@mail.ru>
2025-06-19 18:29:10 +0800

    - io: stream port peer

--- source/iridium/io/implementation/platform/linux/stream_port_acceptor.h
+++ source/iridium/io/implementation/platform/linux/stream_port_acceptor.h
@@ -2,16 +2,31 @@
 #define HEADER_STREAM_PORT_ACCEPTOR_CD2D8A08_F56C_4390_9031_8C06FED4BC75
 
 
+#include "iridium/platform.h"
+
+
+#ifdef LINUX_PLATFORM
+
+
+#include "../unix/stream_port_acceptor.h"
+
+
 namespace iridium {
 namespace io {
 namespace implementation {
 namespace platform {
-namespace linux {
-} // linux
+
+
+typedef platform::unix_::CStreamPortAcceptor CStreamPortAcceptor;
+
+
 } // platform
 } // implementation
 } // io
 } // iridium
+
+
+#endif // MACOS_PLATFORM
 
 
 #endif // HEADER_STREAM_PORT_ACCEPTOR_CD2D8A08_F56C_4390_9031_8C06FED4BC75
--- source/iridium/io/implementation/platform/linux/stream_port_peer.h
+++ source/iridium/io/implementation/platform/linux/stream_port_peer.h
@@ -2,16 +2,31 @@
 #define HEADER_STREAM_PORT_PEER_57A8C17F_23C2_46C3_97DB_22A96F78AEC0
 
 
+#include "iridium/platform.h"
+
+
+#ifdef LINUX_PLATFORM
+
+
+#include "../unix/stream_port_peer.h"
+
+
 namespace iridium {
 namespace io {
 namespace implementation {
 namespace platform {
-namespace linux {
-} // linux
+
+
+typedef platform::unix_::CStreamPortPeer CStreamPortPeer;
+
+
 } // platform
 } // implementation
 } // io
 } // iridium
+
+
+#endif // MACOS_PLATFORM
 
 
 #endif // HEADER_STREAM_PORT_PEER_57A8C17F_23C2_46C3_97DB_22A96F78AEC0
